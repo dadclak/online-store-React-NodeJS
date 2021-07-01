3228fc68354cd92dcb5e6bfdf0489125d38081e8
dadclak <kozanchyn.vladeo@gmail.com>
2021-07-01 11:40:35 +0200

    Creating a storage, UserStore, DeviceStore. Transmitting device and user data.

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
+import { Context } from '..';
 import { authRoutes, publicRoutes } from '../routes';
 import { SHOP_ROUTE } from '../utils/consts';
 
 const AppRouter = () => {
-    const isAuth = false;
+    const {user} = useContext(Context);
+
     return (
         <Switch>
-            { isAuth && authRoutes.map(({path, Component}) =>
+            { user.isAuth && authRoutes.map(({path, Component}) =>
                 <Route key={path} path={path} component={Component} exact />
             )}
             { publicRoutes.map(({path, Component}) =>
