45667df6b6be5723fc9e1d12ea987c527ad29b2f
dadclak <kozanchyn.vladeo@gmail.com>
2021-07-02 22:48:51 +0200

    Implementation of the admin page. Added modal windows with the addition of type, brand and device.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,19 +2,33 @@ import React, {useContext} from 'react';
 import { Context } from '..';
 import { Navbar, Nav, Container, Button} from 'react-bootstrap';
 import {NavLink} from 'react-router-dom'
-import { SHOP_ROUTE } from '../utils/consts';
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from '../utils/consts';
 import { observer } from 'mobx-react-lite';
+import { useHistory } from 'react-router-dom';
 
 const NavBar = observer(() => {
     const {user} = useContext(Context)
+    const history = useHistory();
+
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
                 <NavLink style={{color: 'white'}} to={SHOP_ROUTE}>Hedgehog</NavLink>
                 {user.isAuth ?
                     <Nav className="ml-auto">
-                        <Button variant={'outline-light'}>Admin panel</Button>
-                        <Button variant={'outline-light'} className={'ml-2'}>Sign in</Button>
+                        <Button 
+                            variant={'outline-light'} 
+                            onClick={() => history.push(ADMIN_ROUTE)}
+                        >
+                            Admin panel
+                        </Button>
+                        <Button 
+                            variant={'outline-light'} 
+                            onClick={() => history.push(LOGIN_ROUTE)}
+                            className={'ml-2'}
+                        >
+                            Sign in
+                        </Button>
                     </Nav>
                     :
                     <Nav className="ml-auto" style={{color: 'white'}}>
---
+++ client/src/components/modals/CreateDevice.js
@@ -0,0 +1,105 @@
+import React, { useContext, useState } from 'react';
+import { Modal, Button, Form, Dropdown, Row, Col, FormControl } from 'react-bootstrap';
+import { Context } from '../../index';
+
+const CreateDevice = ({show, onHide}) => {
+    const {device} = useContext(Context)
+    const [info, setInfo] = useState([])
+
+    const addInfo = () => {
+        setInfo([...info, {title: '', description: '', number: Date.now()}])
+    }
+
+    const removeInfo = (number) => {
+        setInfo(info.filter(i => i.number !== number))
+    }
+
+    return (
+        <Modal
+            show={show}
+            onHide={onHide}
+            size="lg"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Add new device
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Dropdown className="mt-2 mb-2">
+                        <Dropdown.Toggle>Select the type</Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            {device.types.map(type =>
+                                <Dropdown.Item key={type.id}>
+                                    {type.name}
+                                </Dropdown.Item>
+                            )}
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    <Dropdown className="mt-2 mb-2">
+                        <Dropdown.Toggle>Select the brand</Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            {device.brands.map(brand =>
+                                <Dropdown.Item key={brand.id}>
+                                    {brand.name}
+                                </Dropdown.Item>
+                            )}
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Enter a name device" 
+                    />
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Enter price device"
+                        type="number" 
+                    />
+                    <Form.Control
+                        className="mt-3"
+                        type="file" 
+                    />
+                    <hr />
+                    <Button
+                        variant={"outline-dark"}
+                        onClick={addInfo}
+                    >
+                        Add new specifications
+                    </Button>
+                    {
+                        info.map(i => 
+                            <Row className="mt-4" key={i.number}>
+                                <Col md={4}>
+                                    <Form.Control
+                                        placeholder="Enter a name specification" 
+                                    />
+                                </Col>
+                                <Col md={4}>
+                                    <Form.Control
+                                        placeholder="Enter a description specification" 
+                                    />
+                                </Col>
+                                <Col md={4}>
+                                    <Button 
+                                        variant={"outline-danger"}
+                                        onClick={(e) => removeInfo(i.number)}
+                                    >
+                                        Delete
+                                    </Button>
+                                </Col>
+                            </Row>
+                        )
+                    }
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="outline-danger" onClick={onHide}>Close</Button>
+                <Button variant="outline-success" onClick={onHide}>Add</Button>
+            </Modal.Footer>
+        </Modal>
+    )
+}
+
+export default CreateDevice;
