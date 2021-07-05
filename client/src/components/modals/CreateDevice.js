82e79154ff48aecb6025653dd6490b9083c43c78
dadclak <kozanchyn.vladeo@gmail.com>
2021-07-05 09:51:16 +0200

    Implementation of adding device characteristics.

--- client/src/components/modals/CreateDevice.js
+++ client/src/components/modals/CreateDevice.js
@@ -1,17 +1,41 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect} from 'react';
 import { Modal, Button, Form, Dropdown, Row, Col } from 'react-bootstrap';
 import { Context } from '../../index';
+import { fetchTypes, fetchBrands } from '../../http/deviceAPI';
+import { observer } from 'mobx-react-lite';
 
-const CreateDevice = ({show, onHide}) => {
+const CreateDevice = observer(({show, onHide}) => {
     const {device} = useContext(Context)
     const [info, setInfo] = useState([])
+    const [name, setName] = useState('')
+    const [price, setPrice] = useState(0)
+    const [file, setFile] = useState(null)
+
+    useEffect(() => {
+        fetchTypes().then(data => device.setTypes(data))
+        fetchBrands().then(data => device.setBrands(data))
+    }, [])
 
     const addInfo = () => {
         setInfo([...info, {title: '', description: '', number: Date.now()}])
     }
 
+    const changeInfo = (key, value, number) => {
+        setInfo(info.map(i => i.number === number ? {...i, [key]: value} : i))
+    }
+
     const removeInfo = (number) => {
         setInfo(info.filter(i => i.number !== number))
+    }
+
+    const selectFile = (e) => {
+        setFile(e.target.files[0])
+    }
+
+    const addDevice = () => {
+        console.log('====================================');
+        console.log(info);
+        console.log('====================================');
     }
 
     return (
@@ -29,20 +53,26 @@ const CreateDevice = ({show, onHide}) => {
             <Modal.Body>
                 <Form>
                     <Dropdown className="mt-2 mb-2">
-                        <Dropdown.Toggle>Select the type</Dropdown.Toggle>
+                        <Dropdown.Toggle>{ device.selectedType.name || 'Select the type'}</Dropdown.Toggle>
                         <Dropdown.Menu>
                             {device.types.map(type =>
-                                <Dropdown.Item key={type.id}>
+                                <Dropdown.Item 
+                                    key={type.id}
+                                    onClick={() => device.setSelectedType(type)}
+                                >
                                     {type.name}
                                 </Dropdown.Item>
                             )}
                         </Dropdown.Menu>
                     </Dropdown>
                     <Dropdown className="mt-2 mb-2">
-                        <Dropdown.Toggle>Select the brand</Dropdown.Toggle>
+                        <Dropdown.Toggle>{ device.selectedBrand.name || 'Select the brand'}</Dropdown.Toggle>
                         <Dropdown.Menu>
                             {device.brands.map(brand =>
-                                <Dropdown.Item key={brand.id}>
+                                <Dropdown.Item 
+                                    key={brand.id}
+                                    onClick={() => device.setSelectedBrand(brand)}
+                                >
                                     {brand.name}
                                 </Dropdown.Item>
                             )}
@@ -50,16 +80,21 @@ const CreateDevice = ({show, onHide}) => {
                     </Dropdown>
                     <Form.Control
                         className="mt-3"
-                        placeholder="Enter a name device" 
+                        placeholder="Enter a name device"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
                     />
                     <Form.Control
+                        type="number"
                         className="mt-3"
                         placeholder="Enter price device"
-                        type="number" 
+                        value={price}
+                        onChange={e => setPrice(Number(e.target.value))} 
                     />
                     <Form.Control
-                        className="mt-3"
                         type="file" 
+                        className="mt-3"
+                        onChange={selectFile}
                     />
                     <hr />
                     <Button
@@ -73,12 +108,16 @@ const CreateDevice = ({show, onHide}) => {
                             <Row className="mt-4" key={i.number}>
                                 <Col md={4}>
                                     <Form.Control
-                                        placeholder="Enter a name specification" 
+                                        placeholder="Enter a name specification"
+                                        value={i.title}
+                                        onChange={e => changeInfo('title', e.target.value, i.number)} 
                                     />
                                 </Col>
                                 <Col md={4}>
                                     <Form.Control
-                                        placeholder="Enter a description specification" 
+                                        placeholder="Enter a description specification"
+                                        value={i.description}
+                                        onChange={e => changeInfo('description', e.target.value, i.number)} 
                                     />
                                 </Col>
                                 <Col md={4}>
@@ -96,10 +135,10 @@ const CreateDevice = ({show, onHide}) => {
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="outline-danger" onClick={onHide}>Close</Button>
-                <Button variant="outline-success" onClick={onHide}>Add</Button>
+                <Button variant="outline-success" onClick={addDevice}>Add</Button>
             </Modal.Footer>
         </Modal>
     )
-}
+});
 
 export default CreateDevice;
